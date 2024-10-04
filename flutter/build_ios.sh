fa5133aea43e455ea0704247e862c303dca63ae4
ITFlexAR <98458686+ITFlexAR@users.noreply.github.com>
2024-10-04 06:37:22 +0200

    build_ios.sh

--- flutter/build_ios.sh
+++ flutter/build_ios.sh
@@ -2,4 +2,4 @@
 # https://docs.flutter.dev/deployment/ios
 # flutter build ipa --release --obfuscate --split-debug-info=./split-debug-info
 # no obfuscate, because no easy to check errors
-flutter build ipa --release
+#flutter build ipa --release
